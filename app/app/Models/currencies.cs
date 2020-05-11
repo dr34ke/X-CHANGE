116e97e83895bdfd6233869124dd9deb13aecde9
Dariusz Kubacki <jack.qeeze@gmail.com>
2020-05-11 13:49:38 +0200

    Dodaj pliki projektów.

---
+++ app/app/Models/currencies.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace app.Models
+{
+    class Table
+    {
+        public string table { get; set; }
+        public string no { get; set; }
+        public string tradingDate { get; set; }
+        public string effectiveDate { get; set; }
+        public Rates rates {get;set;}
+        public Table()
+        {
+
+        }
+    }
+    class Rates
+    {
+
+    }
+}
---
+++ myapp/myapp/Views/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using myapp.Models;
+
+namespace myapp.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : MasterDetailPage
+    {
+        Dictionary<int, NavigationPage> MenuPages = new Dictionary<int, NavigationPage>();
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MasterBehavior = MasterBehavior.Popover;
+        }
+    }
+}
